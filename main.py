f690862e3b9ab447bf497b0eddd9e6157d80d340
Booge2 <vladky54@gmail.com>
2024-04-09 15:20:25 +0300

    Completed work

--- main.py
+++ main.py
@@ -1,16 +1,121 @@
-# This is a sample Python script.
+class MyMetaClass(type):
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __new__(cls, name, bases, dct):
+        if "do_something" not in dct:
+            raise TypeError("Клас повинен мати метод `do_something`")
+
+        dct["do_something"] = decorator(dct["do_something"])
+
+        return super().__new__(cls, name, bases, dct)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def decorator(func):
+    def wrapper(self, *args, **kwargs):
+        print("Перед викликом `do_something`")
+
+        func(self, *args, **kwargs)
+
+        print("Після виклику `do_something`")
+
+    return wrapper
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class MyClass(metaclass=MyMetaClass):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def do_something(self):
+        print("Щось робимо")
+
+
+my_object = MyClass()
+
+my_object.do_something()
+
+print("-----------------------")
+
+
+# Завдання 2
+class MyMetaClass(type):
+
+    def __new__(cls, name, bases, dct, **kwargs):
+        class_name_prefix = kwargs.get("class_name_prefix", None)
+
+        if class_name_prefix:
+            name = f"{class_name_prefix}_{name}"
+
+        return super().__new__(cls, name, bases, dct)
+
+
+class MyClass(metaclass=MyMetaClass, class_name_prefix="My"):
+
+    def do_something(self):
+        print("Щось робимо")
+
+
+my_object = MyClass()
+
+print(type(my_object).__name__)
+
+print("-----------------------")
+
+
+# Завдання 3
+class MyMetaClass(type):
+
+    def __new__(cls, name, bases, dct):
+        dct["author"] = "Your Name"
+
+        dct["version"] = "1.0"
+
+        return super().__new__(cls, name, bases, dct)
+
+
+class MyClass(metaclass=MyMetaClass):
+    pass
+
+
+my_object = MyClass()
+
+print(my_object.author)
+print(my_object.version)
+print("-----------------------")
+
+
+# Завдання 4
+class MyMetaClass(type):
+
+    def __new__(cls, name, bases, dct):
+        init_method = dct["__init__"]
+
+        dct["__init__"] = decorator(init_method)
+
+        return super().__new__(cls, name, bases, dct)
+
+
+def decorator(func):
+    def wrapper(self, *args, **kwargs):
+        if not args:
+            raise TypeError("Необхідно вказати хоча б один аргумент")
+
+        for arg in args:
+            if not isinstance(arg, int):
+                raise TypeError("Аргументи повинні бути типа `int`")
+
+        func(self, *args, **kwargs)
+
+    return wrapper
+
+
+class MyClass(metaclass=MyMetaClass):
+
+    def __init__(self, *args):
+        self.args = args
+
+
+my_object = MyClass(1, 2, 3)
+
+print(my_object.args)
+
+try:
+    my_object = MyClass()
+except TypeError as e:
+    print(e)
